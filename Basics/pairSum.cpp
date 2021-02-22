8c68cd46c7834e497ab178457a1b8f146313e817
AKARSH.H.SIMHA <akarsh07simha@gmail.com>
2021-02-22 10:02:34 +0530

    Pair Sum problem

---
+++ Basics/pairSum.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+bool pairSum(int arr[],int n,int k){
+    int low=0,high=n-1;
+
+    while(low<high){
+        if(arr[low]+arr[high]==k){
+            return true;
+        }
+        else if(arr[low]+arr[high]>k){
+            high--;
+        }
+        else{
+            low++;
+        }
+    }
+    return false;
+}
+
+int main(){
+int n, i,k;
+    cout << "Enter no. of Elements:";
+    cin >> n;
+    
+    int a[n];
+
+    for (i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    cout<<"Enter the Pair Sum:";
+    cin>>k;
+
+    cout<<pairSum(a,n,k);
+}
+
