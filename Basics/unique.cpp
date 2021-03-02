fe641fb37f1fa2d6a40de4fcd64d5c98ab4eece6
AKARSH.H.SIMHA <akarsh07simha@gmail.com>
2021-03-02 07:42:19 +0530

    Finding unique number using XOR

---
+++ Basics/unique.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int unique(int a[],int n){
+    int xorsum=0;
+    for(int i=0;i<n;i++){
+        xorsum=xorsum^a[i];
+    }
+    return xorsum;
+}
+
+int main(){
+    int a[]={2,3,1,4,3,1,2};
+    cout<<(unique(a,7));
+    return 0;
+}
